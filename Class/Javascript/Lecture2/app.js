3d577af21567f92e32afc94fc650cb7c15d4d595
Ahmad Shah <sshah6@stevens.edu>
2024-09-17 20:09:52 -0400

    Calculator App code

--- Class/Javascript/Lecture2/app.js
+++ Class/Javascript/Lecture2/app.js
@@ -5,7 +5,7 @@ try {
     console.log(divideTwoNumbers(5, 4));
     console.log(multiplyTwoNumbers(5, 4));
     console.log(subtractTwoNumbers(5, 4));
-    console.log(divideTwoNumbers('s', 4));
+    console.log(divideTwoNumbers('sss', 4));
 }catch(e){
     console.log(e);
 }
